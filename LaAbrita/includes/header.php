19918d47aa257b7bda2a00fb3186a2a313da73c2
AlvaroThg <125067457+AlvaroThg@users.noreply.github.com>
2025-02-04 21:09:31 -0400

    Add files via upload

---
+++ LaAbrita/includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zonova: Tu Inmobiliaria</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="navbar-container">
+                <div class="logo">
+                    <img src="/images/logo_conz.png" alt="Logo Inmobiliaria Zonoba">
+                </div>
+                <div class="nav-links-container">
+                    <ul class="nav-links">
+                        <li><a href="../index.php">Inicio</a></li>
+                        <li><a href="../about.php">Sobre Nosotros</a></li>
+                        <li><a href="../agents.php">Agentes</a></li>
+                        <li><a href="../properties.php">Propiedades</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ LaAbrita/properties.php
@@ -0,0 +1,43 @@
+<?php include 'includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de Nosotros - Zonoba</title>
+
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles-properties.css">
+</head>
+    <body>
+        <!-- Sección de propiedades -->
+        <section class="properties">
+                <div class="property">
+                    <img src="/images/property1.jpg" alt="Propiedad 1">
+                    <div class="property-info">
+                        <h3>Casa Familiar</h3>
+                        <p>Ubicación ideal para familias, cerca de parques y escuelas.</p>
+                        <a href="data/casa-familiar.php" class="cta-button">Revisar Propiedad</a>
+                    </div>
+                </div>
+                <div class="property">
+                    <img src="/images/property2.jpg" alt="Propiedad 2">
+                    <div class="property-info">
+                        <h3>Departamento Moderno</h3>
+                        <p>Estilo contemporáneo, excelente para parejas jóvenes.</p>
+                        <a href="data/dpto-moderno.php" class="cta-button">Revisar Propiedad</a>
+                    </div>
+                </div>
+                <div class="property">
+                    <img src="/images/property3.jpg" alt="Propiedad 3">
+                    <div class="property-info">
+                        <h3>Oficina Espaciosa</h3>
+                        <p>Zona céntrica, ideal para negocios.</p>
+                        <a href="data/oficina-espaciosa.php" class="cta-button">Revisar Propiedad</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
