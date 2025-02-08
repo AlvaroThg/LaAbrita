285ad5fe60c91b499a435bc9ded0bc24c9c98f55
maycro <juan.cordeiro@ucb.edu.bo>
2025-02-07 23:00:34 -0400

    Mejora Visual La Abrita 23:00 07/02/2025

--- includes/header.php
+++ includes/header.php
@@ -1,53 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La Abrita: Rutas hacia tu interior</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
     <header>
         <div class="header-image">
             <div class="overlay"></div>
             <div class="header-content">
-                <h1>La Abrita</h1>
-                <p>Vive las rutas hacia tu interior</p>
+
+                <!-- Reemplazamos el h1 con la imagen -->
+                <img src="../images/logoAbritaCentradoSINLUNA.jpg" alt="La Abrita" class="logo">
+                <img src="../images/arbitaLuna.jpg" alt="Luna" class="luna">
+                
                 <nav class="nav-links-container">
                     <ul class="nav-links">
                         <li><a href="../index.php">Inicio</a></li>
                         <li><a href="../about.php">Sobre Nosotros</a></li>
-                        <li><a href="../content.php">Contenido</a></li>
+                        <li><a href="../content.php">Galería</a></li>
                     </ul>
                 </nav>
+                
             </div>
-            <!-- Imágenes de hojas -->
-            <img src="../images/hoja1.png" alt="Hoja 1" class="hoja-esquina hoja-inferior-izquierda">
-            <img src="../images/hoja2.png" alt="Hoja 2" class="hoja-esquina hoja-superior-derecha">
         </div>
     </header>
 </body>
+
 </html>
-<script>
-    window.addEventListener("scroll", function() {
-        let hojas = document.querySelectorAll(".hoja-esquina");
-        let screenHeight = window.innerHeight;
-
-        hojas.forEach(function(hoja) {
-            let position = hoja.getBoundingClientRect().top;
-            if (position < screenHeight - 100) {
-                hoja.classList.add("visible");
-
-                // Aplicar el efecto después de 2 segundos
-                setTimeout(() => {
-                    hoja.classList.add("pequena");
-
-                    // Volver a hacer el efecto de florecer después de 1 segundo
-                    setTimeout(() => {
-                        hoja.classList.remove("pequena");
-                    }, 1000);
-                }, 2000);
-            }
-        });
-    });
-</script>
