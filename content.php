1e1ad2b07d0d1fcdcb872a291683a7bf6c893ad8
AlvaroTHG <alvarobaldiviezo8@gmail.com>
2025-02-17 11:57:21 -0400

    Juan, Haz lo tuyo 17/2/25 11:57

--- content.php
+++ content.php
@@ -4,40 +4,86 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acerca de Nosotros - Zonoba</title>
+    <title>Galeria - La Abrita</title>
 
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/styles-properties.css">
+    <link rel="stylesheet" href="css/styles-content.css">
 </head>
-    <body>
-        <!-- Sección de propiedades -->
-        <section class="properties">
-                <div class="property">
-                    <img src="/images/property1.jpg" alt="Propiedad 1">
-                    <div class="property-info">
-                        <h3>Casa Familiar</h3>
-                        <p>Ubicación ideal para familias, cerca de parques y escuelas.</p>
-                        <a href="data/casa-familiar.php" class="cta-button">Revisar Propiedad</a>
-                    </div>
-                </div>
-                <div class="property">
-                    <img src="/images/property2.jpg" alt="Propiedad 2">
-                    <div class="property-info">
-                        <h3>Departamento Moderno</h3>
-                        <p>Estilo contemporáneo, excelente para parejas jóvenes.</p>
-                        <a href="data/dpto-moderno.php" class="cta-button">Revisar Propiedad</a>
-                    </div>
-                </div>
-                <div class="property">
-                    <img src="/images/property3.jpg" alt="Propiedad 3">
-                    <div class="property-info">
-                        <h3>Oficina Espaciosa</h3>
-                        <p>Zona céntrica, ideal para negocios.</p>
-                        <a href="data/oficina-espaciosa.php" class="cta-button">Revisar Propiedad</a>
-                    </div>
-                </div>
-            </section>
-        </main>
-        <?php include 'includes/footer.php'; ?>
-    </body>
+<body>
+    <section class="contents">
+        <div class="content large">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Vino">
+                <h3>Vino</h3>
+                <p>Sabor Frambuesa/Uva.</p>
+                <a href="data/casa-familiar.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+        <div class="content medium">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Mermelada">
+                <h3>Mermelada</h3>
+                <p>Sabor Mora Azul.</p>
+                <a href="data/dpto-moderno.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+        <div class="content small">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Uva">
+                <h3>Uva</h3>
+                <p>Venta a por mayor.</p>
+                <a href="data/oficina-espaciosa.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+        <div class="content large">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Taller de Autosuficiencia">
+                <h3>Taller de Autosuficiencia</h3>
+                <p>Aprende a cultivar frutas y verduras, y a cocinar lo que cosechas.</p>
+                <a href="data/casa-campo.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+        <div class="content medium">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Hospedaje">
+                <h3>Hospedaje</h3>
+                <p>Espacioso y comodo.</p>
+                <a href="data/loft-urbano.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+        <div class="content small">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Local Comercial">
+                <h3>EJ1</h3>
+                <p>Ubicación.</p>
+                <a href="data/local-comercial.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+        <div class="content large">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Penthouse de Lujo">
+                <h3>Ej2</h3>
+                <p>Vistas panorámicas.</p>
+                <a href="data/penthouse-lujo.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+        <div class="content medium">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Apartamento Familiar">
+                <h3>Ej3</h3>
+                <p>Amplio y cómodo.</p>
+                <a href="data/apartamento-familiar.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+        <div class="content small">
+            <div class="content-info">
+                <img src="/images/abritaImg1.png" alt="Bodega Industrial">
+                <h3>Ej4</h3>
+                <p>Espacio ideal para almacenamiento.</p>
+                <a href="data/bodega-industrial.php" class="cta-button">Contactar</a>
+            </div>
+        </div>
+    </section>
+    <?php include 'includes/footer.php'; ?>
+</body>
 </html>
