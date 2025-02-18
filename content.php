f4503ff4a16e6b454bad9c87ef2f68876872fd9c
maycro <juan.cordeiro@ucb.edu.bo>
2025-02-17 21:49:28 -0400

    Actualizacion Galería 1 Abrita

--- content.php
+++ content.php
@@ -16,7 +16,7 @@
                 <img src="/images/abritaImg1.png" alt="Vino">
                 <h3>Vino</h3>
                 <p>Sabor Frambuesa/Uva.</p>
-                <a href="data/casa-familiar.php" class="cta-button">Contactar</a>
+
             </div>
         </div>
         <div class="content medium">
@@ -24,7 +24,7 @@
                 <img src="/images/abritaImg1.png" alt="Mermelada">
                 <h3>Mermelada</h3>
                 <p>Sabor Mora Azul.</p>
-                <a href="data/dpto-moderno.php" class="cta-button">Contactar</a>
+     
             </div>
         </div>
         <div class="content small">
@@ -32,7 +32,7 @@
                 <img src="/images/abritaImg1.png" alt="Uva">
                 <h3>Uva</h3>
                 <p>Venta a por mayor.</p>
-                <a href="data/oficina-espaciosa.php" class="cta-button">Contactar</a>
+ 
             </div>
         </div>
         <div class="content large">
@@ -40,7 +40,7 @@
                 <img src="/images/abritaImg1.png" alt="Taller de Autosuficiencia">
                 <h3>Taller de Autosuficiencia</h3>
                 <p>Aprende a cultivar frutas y verduras, y a cocinar lo que cosechas.</p>
-                <a href="data/casa-campo.php" class="cta-button">Contactar</a>
+
             </div>
         </div>
         <div class="content medium">
@@ -48,7 +48,7 @@
                 <img src="/images/abritaImg1.png" alt="Hospedaje">
                 <h3>Hospedaje</h3>
                 <p>Espacioso y comodo.</p>
-                <a href="data/loft-urbano.php" class="cta-button">Contactar</a>
+
             </div>
         </div>
         <div class="content small">
@@ -56,7 +56,7 @@
                 <img src="/images/abritaImg1.png" alt="Local Comercial">
                 <h3>EJ1</h3>
                 <p>Ubicación.</p>
-                <a href="data/local-comercial.php" class="cta-button">Contactar</a>
+
             </div>
         </div>
         <div class="content large">
@@ -64,7 +64,7 @@
                 <img src="/images/abritaImg1.png" alt="Penthouse de Lujo">
                 <h3>Ej2</h3>
                 <p>Vistas panorámicas.</p>
-                <a href="data/penthouse-lujo.php" class="cta-button">Contactar</a>
+
             </div>
         </div>
         <div class="content medium">
@@ -72,7 +72,7 @@
                 <img src="/images/abritaImg1.png" alt="Apartamento Familiar">
                 <h3>Ej3</h3>
                 <p>Amplio y cómodo.</p>
-                <a href="data/apartamento-familiar.php" class="cta-button">Contactar</a>
+
             </div>
         </div>
         <div class="content small">
@@ -80,7 +80,7 @@
                 <img src="/images/abritaImg1.png" alt="Bodega Industrial">
                 <h3>Ej4</h3>
                 <p>Espacio ideal para almacenamiento.</p>
-                <a href="data/bodega-industrial.php" class="cta-button">Contactar</a>
+
             </div>
         </div>
     </section>
